35e2352361614332c1c633c02a73a5a79b4cc97f
Binay <binay.kumar@smartcoin.co.in>
2024-11-21 20:04:48 +0530

    added some programe

--- selfquestions/find the duplicate and its index.py
+++ selfquestions/find the duplicate and its index.py
@@ -1,27 +1,29 @@
 # input="I am an Indian"
 # "IamanIndian"
+input1 = str(input("ENter a string: "))
 
-str = str(input("enter the string any: "))
-
-newstring = str.split()
-newstring1 = ''.join(newstring)
-print(newstring1)
+newstr = input1.split()
+newstr1 = ''.join(newstr)
+print(newstr1)
 d = {}
-for i in newstring1.lower():
+for i in newstr1:
     if i in d:
         d[i] += 1
-
     else:
         d[i] = 1
-print("dict is:", d)
+print(d)
 l = []
 for i in d:
     if d[i] >= 2:
         l.append(i)
+print(l)
+indexlist = []
+d1 = {}
+for i in range(len(input)):
+    if input[i] in l:
+        if input[i] in d1:
+            d1[input[i]].append(i)
+        else:
+            d1[input[i]] = [i]
 
-print("repeating char is:", l)
-index1=[]
-for i in range(len(newstring1)):
-    if newstring1[i].lower() in l:
-        index1.append(i)
-print(index1)
+print(d1)
